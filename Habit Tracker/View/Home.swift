77cc5e354d9c38bcf94a873e104b6298923ad0a8
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-05-27 20:50:43 -0600

    add habit card view

--- Habit Tracker/View/Home.swift
+++ Habit Tracker/View/Home.swift
@@ -25,11 +25,19 @@ struct Home: View {
                             .foregroundColor(.white)
                     }
                 }
+                .padding(.bottom, 10)
             
             ScrollView(habits.isEmpty ? .init() : .vertical, showsIndicators: false) {
                 VStack (spacing: 15) {
+                    
+                    VStack {
+                        ForEach(habits) { habit in
+                            HabitCardView(habit: habit)
+                        }
+                    }
+                    
                     Button {
-                        
+                        viewModel.addNewHabit.toggle()
                     } label: {
                         Label {
                             Text("New Habit")
@@ -54,6 +62,87 @@ struct Home: View {
         }
         .frame(maxHeight: .infinity, alignment: .top)
         .padding()
+    }
+    
+    @ViewBuilder
+    func HabitCardView(habit: Habit) -> some View {
+        VStack(spacing: 6) {
+            HStack {
+                Text(habit.title ?? "")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                
+                Image(systemName: "bell.badge.fill")
+                    .font(.callout)
+                    .foregroundColor(Color(habit.color ?? "Card-1"))
+                    .scaleEffect(0.9)
+                    .opacity(habit.isRemainderOn ? 1 : 0)
+                
+                Spacer()
+                
+                let count = (habit.weekDays?.count ?? 0)
+                Text(count == 7 ? "Everyday" : "\(count) times a week")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .padding(.horizontal, 10)
+            
+            let calendar = Calendar.current
+            let currentWeek = calendar.dateInterval(of: .weekOfMonth, for: Date())
+            let symbols = calendar.weekdaySymbols
+            let startDate = currentWeek?.start ?? Date()
+            let activeWeekDays = habit.weekDays ?? []
+            let activePlot = symbols.indices.compactMap { index -> (String, Date) in
+                let currentDate = calendar.date(byAdding: .day, value: index, to: startDate)
+                return (symbols[index], currentDate!)
+            }
+            
+            HStack(spacing: 0) {
+                ForEach(activePlot.indices, id: \.self) { index in
+                    let item = activePlot[index]
+                    
+                    VStack(spacing: 6) {
+                        Text(item.0.prefix(3))
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        let status = activeWeekDays.contains { day in
+                            return day == item.0
+                        }
+                        
+                        Text(getDate(date: item.1))
+                            .font(.system(size: 14))
+                            .fontWeight(.semibold)
+                            .padding(8)
+                            .background {
+                                Circle()
+                                    .fill(Color(habit.color ?? "Card-1"))
+                                    .opacity(status ? 1 : 0)
+                            }
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(.top, 15)
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 6)
+        .background {
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(Color("TFBG").opacity(0.5))
+        }
+        .onTapGesture {
+            viewModel.addNewHabit.toggle()
+        }
+    }
+    
+    private func getDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd"
+        
+        return formatter.string(from: date)
     }
 }
 
